70167ae91bc4cdfa0227f3ccab3a0df484fbcd0f
Aviral Gupta <aviralg@codeaurora.org>
2014-01-16 14:31:06 -0800

    ASoC: mpq8092: Add the passthrough support in the compressed driver
    Compressed passthrough gives the user the flexibility to decode the audio
    data on end device.
    Configure the ADSP so that the data gets passthrough without decoding.
    
    Change-Id: I6bd073d55c14d4ed6f7c67829759aef3655eef82
    Signed-off-by: Aviral Gupta <aviralg@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -108,6 +108,7 @@ struct msm_compr_audio {
 	struct audio_client *audio_client;
 
 	uint32_t codec;
+	uint32_t compr_passthr;
 	void    *buffer; /* virtual address */
 	uint32_t buffer_paddr; /* physical address */
 	uint32_t app_pointer;
@@ -167,6 +168,8 @@ struct msm_compr_sony_hweffect {
 struct msm_compr_dec_params {
 	struct snd_dec_ddp ddp_params;
 };
+
+u32 compr_codecs[] = {SND_AUDIOCODEC_AC3};
 
 static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 				uint32_t volume_l, uint32_t volume_r)
@@ -614,25 +617,47 @@ static int msm_compr_configure_dsp(struct snd_compr_stream *cstream)
 
 	prtd->gapless_state.stream_opened[stream_index] = 1;
 	pr_debug("%s be_id %d\n", __func__, soc_prtd->dai_link->be_id);
-	msm_pcm_routing_reg_phy_stream(soc_prtd->dai_link->be_id,
+
+	if (prtd->compr_passthr == true) {
+		ret = q6asm_open_write_compressed(ac, prtd->codec);
+		if (ret < 0) {
+			pr_err("%s: Session out open failed\n", __func__);
+			return -ENOMEM;
+		}
+		msm_pcm_routing_reg_phy_compr_stream(
+				soc_prtd->dai_link->be_id,
+				ac->perf_mode,
+				prtd->session_id,
+				SNDRV_PCM_STREAM_PLAYBACK,
+				prtd->compr_passthr);
+	} else {
+		ret = q6asm_stream_open_write_v2(ac,
+				prtd->codec, bits_per_sample,
+				ac->stream_id, true/*gapless*/);
+		if (ret < 0) {
+			pr_err("%s: Session out open failed\n", __func__);
+			return -ENOMEM;
+		}
+
+		msm_pcm_routing_reg_phy_stream(soc_prtd->dai_link->be_id,
 				ac->perf_mode,
 				prtd->session_id,
 				SNDRV_PCM_STREAM_PLAYBACK);
 
-	/*
-	 * Setting the master volume gain to 0 while
-	 * configuring ASM session. This is to address
-	 * DSP pop noise issue where. This change is
-	 * there from begining may be DSP limitation
-	 */
-	ret = msm_compr_set_volume(cstream, 0, 0);
-	if (ret < 0)
-		pr_err("%s : Set Volume failed : %d", __func__, ret);
+		ret = msm_compr_set_volume(cstream, 0, 0);
+		if (ret < 0)
+			pr_err("%s : Set Volume failed : %d", __func__, ret);
 
-	ret = q6asm_set_softvolume(ac, &softvol);
-	if (ret < 0)
-		pr_err("%s: Send SoftVolume Param failed ret=%d\n",
-			__func__, ret);
+		ret = q6asm_set_softpause(ac, &softpause);
+		if (ret < 0)
+			pr_err("%s: Send SoftPause Param failed ret=%d\n",
+					__func__, ret);
+
+		ret = q6asm_set_softvolume(ac, &softvol);
+		if (ret < 0)
+			pr_err("%s: Send SoftVolume Param failed ret=%d\n",
+					__func__, ret);
+	}
 
 	ret = q6asm_set_softpause(ac, &softpause);
 	if (ret < 0)
@@ -869,6 +894,15 @@ static int msm_compr_free(struct snd_compr_stream *cstream)
 	return 0;
 }
 
+int validate_codec_compr(__u32 codec_id)
+{
+	int i;
+	for (i = 0; i < ARRAY_SIZE(compr_codecs); i++)
+		if (compr_codecs[i] == codec_id)
+			return 0;
+	return -EINVAL;
+}
+
 /* compress stream operations */
 static int msm_compr_set_params(struct snd_compr_stream *cstream,
 				struct snd_compr_params *params)
@@ -884,6 +918,8 @@ static int msm_compr_set_params(struct snd_compr_stream *cstream,
 
 	/* ToDo: remove duplicates */
 	prtd->num_channels = prtd->codec_param.codec.ch_in;
+
+	prtd->compr_passthr = params->compr_passthr;
 
 	switch (prtd->codec_param.codec.sample_rate) {
 	case SNDRV_PCM_RATE_8000:
@@ -909,8 +945,11 @@ static int msm_compr_set_params(struct snd_compr_stream *cstream,
 		prtd->sample_rate = 48000;
 		break;
 	}
-
-	pr_debug("%s: sample_rate %d\n", __func__, prtd->sample_rate);
+	if (prtd->compr_passthr && validate_codec_compr(params->codec.id)) {
+		pr_err("%s codec not supported in passthrough, id =%d\n",
+				__func__, params->codec.id);
+		return -EINVAL;
+	}
 
 	switch (params->codec.id) {
 	case SND_AUDIOCODEC_PCM: {
