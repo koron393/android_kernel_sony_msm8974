9c4b6bc9f1031bb48df6b1b3ded5e453f2c3d2e4
Remilia Scarlet <remilia.1505@gmail.com>
2018-04-02 00:51:28 +0700

    msm8974: Add build scripts
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

---
+++ kernel_zip/tools/flash_kernel.sh
@@ -0,0 +1,20 @@
+#!/sbin/sh
+#
+# Live ramdisk patching script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+
+cd /tmp/
+/sbin/busybox dd if=/dev/block/mmcblk0p14 of=./boot.img
+./unpackbootimg -i /tmp/boot.img
+./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1 vmalloc=300M dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y" --base 0x00000000 --pagesize 2048 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --dt /tmp/boot.img-dt -o /tmp/newboot.img
+/sbin/busybox dd if=/tmp/newboot.img of=/dev/block/mmcblk0p14
