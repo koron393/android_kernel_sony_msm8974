042a235184605c40777ff0faec72f87d8e313dae
Remilia Scarlet <remilia.1505@gmail.com>
2018-04-14 21:15:08 +0700

    ramdisk-RZ: Move to system
    We have problems patching ramdisk with Magisk boot image so let's
    move our own power commands into /etc/init (Nougat supports init
    init scripts on system partition)
    
    Signed-off-by: Remilia Scarlet <remilia.1505@gmail.com>

--- kernel_zip/tools/flash_kernel.sh
+++ kernel_zip/tools/flash_kernel.sh
@@ -27,21 +27,8 @@ pagesize=$(cat *-pagesize);
 ramdisk_offset=$(cat *-ramdisk_offset);
 tags_offset=$(cat *-tags_offset);
 
-## Extract ramdisk
-mkdir -p /tmp/ramdisk;
-cd /tmp/ramdisk;
-# Here we only support gzip ramdisk, as it's begin widely used
-gunzip -c ../boot.img-ramdisk.gz | $bb cpio -i;
-
-# Pack ramdisk with RZ's extra rc file
-cp -f /tmp/ramdisk-RZ/* /tmp/ramdisk;
-sed -i 's:init.qcom.power.rc:init.RZ.rc:g' /tmp/ramdisk/init.qcom.rc;
-cd /tmp;
-./mkbootfs /tmp/ramdisk/ > /tmp/boot.img-ramdisk;
-cat /tmp/boot.img-ramdisk | gzip > /tmp/boot.img-ramdisk.gz
-
 # Pack our new boot.img
-./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$cmdline androidboot.selinux=permissive" --base $base --pagesize $pagesize --ramdisk_offset /tmp/ramdisk_offset --tags_offset $tags_offset --dt /tmp/dt.img -o /tmp/newboot.img;
+./mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$cmdline androidboot.selinux=permissive" --base $base --pagesize $pagesize --ramdisk_offset $ramdisk_offset --tags_offset $tags_offset --dt /tmp/dt.img -o /tmp/newboot.img;
 
 # It's flashing time!!
 dd if=/tmp/newboot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot;
